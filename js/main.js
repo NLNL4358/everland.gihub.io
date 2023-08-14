56ba19fc2d3e141c520f6bbc7b40c078df35a5c1
sohui <sohi0000@naver.com>
2023-08-14 14:18:18 +0900

    20230814

--- js/main.js
+++ js/main.js
@@ -7,7 +7,9 @@ var swiper = new Swiper(".main_slide", {
     disableOnInteraction: false,
   },
   pagination: {
-    el: ".swiper-pagination",
+    el: ".main_slide .swiper-pagination",
     clickable: true,
   },
 });
+/* 날씨 */
+let apiURI = 'http://api.openweathermap.org/data/2.5/weather?q=Daejeon&appid=2821934c9ad6c1c2617558f2c234c481';
