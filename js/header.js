bbc6b8df515cf33fc6f3f191231df32584d5899d
LetsGo <wide4514@gmail.com>
2023-08-14 11:37:10 +0900

    header - 폰트조정, 트랜지션 확인필요 2023.08.14

--- js/header.js
+++ js/header.js
@@ -4,8 +4,14 @@ $(document).ready(function(){
   let gnbLi = document.querySelectorAll(".gnb_li")
   let snbUl = document.querySelectorAll(".snb_ul");
 
+  let searchButton = document.querySelector(".search_button"); /* 데스크탑 서치버튼 */
+  let modalSearchBar = document.querySelector(".modal_search_bar");  /* 데스크탑 모달 서치바 */
+  let searchBarSearchButton = document.querySelector(".search_bar_search_button"); /* 검색 서치 버튼 */
+  let searchBarCloseButton = document.querySelector(".search_bar_close_button"); /* 데스크탑 서치 x버튼  */
+
+
   /* 이벤트리스너 */
-  gnbLi.forEach((item, index) => {
+  gnbLi.forEach((item, index) => {  /* Nav */
     $(gnbLi[index]).mouseover(function(event){
       gnbLi.forEach((itemA, indexA)=>{
         gnbLi[indexA].classList.remove("on");
@@ -19,10 +25,24 @@ $(document).ready(function(){
     })
   });
 
+  searchButton.addEventListener("click", function(e){ /* SearchButton */
+    toggleSearchBar();
+  })
+  searchBarCloseButton.addEventListener("click", function(e){
+    toggleSearchBar();
+  })
+  searchBarSearchButton.addEventListener("click", function(e){
+    toggleSearchBar();
+  })
 
   /* 함수 */
-
-
+  function toggleSearchBar(){
+    $(modalSearchBar).toggleClass("visible");
+  }
+  function toggleSearchBar(){
+    $(modalSearchBar).toggleClass("visible");
+  }
+  
   /* 초기화 */
   gnbLi.forEach((item,index)=>{
     gnbLi[index].classList.remove("on");
