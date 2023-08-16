162ef7c1e0d1eaff7d04de3f86bf4fa87147e9db
sohui <sohi0000@naver.com>
2023-08-16 13:29:41 +0900

    20230816

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ var swiper = new Swiper(".main_slide", {
 });
 
 /* 날씨 */
-let weatherIcon = {
+let weatherIcon = { // 날씨아이콘 변경
   '01' : 'clear_sky',
   '02' : 'few_cloud',
   '03' : 'cloud',
@@ -28,7 +28,7 @@ let weatherIcon = {
 const xhr = new XMLHttpRequest();
 const getJson = function(url, callback) {
   xhr.open("GET", url, true);
-  xhr.responseType = "json";  // 오타 수정
+  xhr.responseType = "json"; 
   xhr.onload = function() {
     const status = xhr.status;
     if (status === 200) {
@@ -45,9 +45,9 @@ getJson("http://api.openweathermap.org/data/2.5/weather?q=seoul&appid=2821934c9a
   } else {
     let CurrTemp = document.querySelector('.CurrTemp span');
     let CurrIcon = document.querySelector('.CurrIcon img');
-    CurrTemp.innerText = Math.floor(data.main.temp);
+    CurrTemp.innerText = Math.floor(data.main.temp); // 온도 소수점 없이
     let Icon = (data.weather[0].icon).substr(0,2);
-    let imgURL = `img/weather/${weatherIcon[Icon]}.png`;
+    let imgURL = `img/weather/${weatherIcon[Icon]}.png`; // 날씨 아이콘 이미지
     CurrIcon.setAttribute("src", imgURL);
   }
 });
@@ -77,6 +77,180 @@ mapMadalOpen.addEventListener("click", function() {
 close.addEventListener("click", function() {
   mapMadal.style.display = "none"; // 모달 닫기
 });
+
+/* timetable */
+let timetable = document.querySelector(".timetable");
+const timetableArray = [
+  {
+    _id : "1",
+    name : "씨라이언 빌리지 애니멀톡",
+    time : "13:00",
+  },
+  {
+    _id : "2",
+    name : "장미성 뮤직 워터 BaMM",
+    time : "13:00",
+  },
+  {
+    _id : "3",
+    name : "슈퍼윙스 애니멀 톡",
+    time : "13:30",
+  },
+  {
+    _id : "4",
+    name : "슈팅워터펀&밤밤클럽",
+    time : "13:30",
+  },
+  {
+    _id : "5",
+    name : "씨라이언 빌리지 애니멀톡",
+    time : "14:00",
+  },
+  {
+    _id : "6",
+    name : "장미성 뮤직 워터 BaMM",
+    time : "14:00",
+  },
+  {
+    _id : "7",
+    name : "슈퍼윙스 애니멀 톡",
+    time : "14:30",
+  },
+  {
+    _id : "8",
+    name : "알버트 라이브 톡",
+    time : "14:30",
+  },
+  {
+    _id : "9",
+    name : "씨라이언 빌리지 애니멀톡",
+    time : "15:00",
+  },
+  {
+    _id : "10",
+    name : "장미성 뮤직 워터 BaMM",
+    time : "15:00",
+  },
+  {
+    _id : "11",
+    name : "뿌빠 뱀과 육지거북 애니멀톡",
+    time : "15:00",
+  },
+  {
+    _id : "12",
+    name : "레니의 대모험~ 드래곤 성을 찾아서~",
+    time : "15:00",
+  },
+  {
+    _id : "13",
+    name : "슈퍼윙스 애니멀 톡",
+    time : "15:30",
+  },
+  {
+    _id : "14",
+    name : "알버트 라이브 톡",
+    time : "15:30",
+  },
+  {
+    _id : "15",
+    name : "씨라이언 빌리지 애니멀톡",
+    time : "16:00",
+  },
+  {
+    _id : "16",
+    name : "장미성 뮤직 워터 BaMM",
+    time : "16:00",
+  },
+  {
+    _id : "17",
+    name : "뿌빠 사막여우 애니멀톡",
+    time : "16:00",
+  },
+  {
+    _id : "18",
+    name : "슈퍼윙스 애니멀 톡",
+    time : "16:30",
+  },
+  {
+    _id : "19",
+    name : "슈팅워터펀&밤밤클럽",
+    time : "16:30",
+  },
+  {
+    _id : "20",
+    name : "장미성 뮤직 워터 BaMM",
+    time : "17:00",
+  },
+  {
+    _id : "21",
+    name : "레니의 대모험~ 드래곤 성을 찾아서~",
+    time : "17:30",
+  },
+  {
+    _id : "22",
+    name : "장미성 뮤직 워터 BaMM",
+    time : "18:00",
+  },
+  {
+    _id : "23",
+    name : "장미성 뮤직 워터 BaMM",
+    time : "19:00",
+  },
+  {
+    _id : "24",
+    name : "문라이트 퍼레이드",
+    time : "20:30",
+  },
+  {
+    _id : "25",
+    name : "가든 오브 라이츠 with SHINee",
+    time : "21:00",
+  },
+  {
+    _id : "26",
+    name : "EVERTOPIA - The Origin of Everland",
+    time : "21:30",
+  },
+]
+
+const timetableArrPro = timetableArray.map(item => {
+  return {
+    name: item.name,
+    time: item.time,
+  }
+});
+
+timetableArrPro.forEach(item => {
+  const listItem = document.createElement('li'); // 새로운 <li> 요소 생성
+  listItem.innerHTML = `<span class="timetable_time">${item.time}</span><span class = "yellow_box"></span><span class="timetable_name">${item.name}</span>`; // HTML 내용 설정
+  timetable.appendChild(listItem); // 생성한 <li> 요소를 .timetable에 추가
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /* 지도 */
 let mapContainer = document.getElementById("kakao_map");
